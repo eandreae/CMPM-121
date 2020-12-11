6e59dec2e9e3f08d15e3edb68903ec290aa48b59
eandreae <eandreae@ucsc.edu>
2020-12-11 00:08:39 -0500

    Added Road Models
    Added 4 pieces of road models:
    - Corner Intersection
    - 3 Way Intersection
    - 4 Way Intersection
    - Flat Road
    Added working traffic lights for all intersections.

---
+++ Taxi Game/Assets/WheelAnimation.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class WheelAnimation : MonoBehaviour
+{
+
+    private Animation anim;
+    public GameObject speedTracker;
+    private float speed = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animation>();
+        anim["Front Forward"].layer = 123;
+        anim["Front Reverse"].layer = 123;
+    }
+
+    float getSpeed() {
+        var transform = speedTracker.GetComponent<Transform>();
+        var position = transform.position;
+        return position.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ( anim.isPlaying) {
+            return;
+        }
+
+        speed = getSpeed();
+
+        if ( speed > 0.0f ){
+            Debug.Log("Moving Forward");
+            anim.Play("Front Forward");
+            anim.Play("Rear Forward");
+        }
+
+        if ( speed < 0.0f ){
+            Debug.Log("Moving Backward");
+            anim.Play("Front Reverse");
+            anim.Play("Rear Reverse");
+        }
+    }
+}
---
+++ Taxi Game/Assets/player.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class player : MonoBehaviour
+{
+
+    public float speed        = 0.0f;
+    private float acceleration = 10.0f;
+    private float rotateSpeed  = 0.6f;
+    private float MAX_SPEED = 30.0f;
+
+    public TextMeshProUGUI speedometer;
+    public GameObject speedTracker;
+    public GameObject rearLights;
+    public GameObject reverseLights;
+
+    public void updateSpeedometer() {
+        float speedData = Mathf.Floor(this.speed * 2.0f);
+        this.speedometer.text = speedData.ToString();
+    }
+
+    public void updateSpeedTracker() {
+        var transform = speedTracker.GetComponent<Transform>();
+        var position = transform.position;
+        position.x = this.speed;
+        transform.position = position;
+    }
+
+    public void switchOn(GameObject lights) {
+        if(lights.activeSelf == false){lights.SetActive(true);}
+    }
+
+    public void switchOff(GameObject lights) {
+        if(lights.activeSelf == true){lights.SetActive(false);}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // From the Unity documentation on simplemove.
+        CharacterController controller = GetComponent<CharacterController>();
+
+        transform.Rotate(
+            0.0f, 
+            Input.GetAxis("Horizontal") * this.rotateSpeed,
+            0.0f
+        );
+
+        if ( Input.GetAxis("Vertical") > 0 && this.speed < MAX_SPEED ){
+            Debug.Log("Accelerating");
+
+            if ( this.speed >= 0 ){this.speed += acceleration * Time.deltaTime;}
+            if ( this.speed < 0 ){
+                this.speed += acceleration * Time.deltaTime * 2.0f;
+            }
+            switchOff(rearLights);
+            switchOff(reverseLights);
+            
+        }
+
+        if (Input.GetAxis("Vertical") == 0 ){
+            Debug.Log("Decelerating");
+
+            if ( this.speed > 0 ) {
+                this.speed -= acceleration/1.25f * Time.deltaTime;
+                if ( this.speed < 1 ){ this.speed = 0; }
+                switchOff(rearLights);
+                switchOff(reverseLights);
+            }
+            if ( this.speed < 0 ) {
+                this.speed += acceleration/1.25f * Time.deltaTime;
+                if ( this.speed > 1 ) { this.speed = 0; }
+                switchOff(rearLights);
+                switchOff(reverseLights);
+            }
+        }
+
+        if ( Input.GetAxis("Vertical") < 0 && this.speed > -18f){
+            Debug.Log("Reversing");
+            if ( this.speed <= 0 ){this.speed -= acceleration/2.0f * Time.deltaTime;}
+            if ( this.speed > 0 ){
+                this.speed -= acceleration * Time.deltaTime * 2.0f;
+            }
+            if (this.speed < 0.0f ){
+                switchOff(rearLights);
+                switchOn(reverseLights);
+            }
+            else {
+                switchOff(reverseLights);
+                switchOn(rearLights);
+            }
+        }
+
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        float curSpeed = this.speed;
+        controller.SimpleMove(forward * curSpeed);
+
+        updateSpeedTracker();
+        updateSpeedometer();
+        
+        
+    }
+
+    
+}
